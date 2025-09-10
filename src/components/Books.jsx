85bf6c85a3bf37c19752c199193086f1636a7b41
Gabriel <glupateli@gmail.com>
2025-09-10 19:35:45 -0300

    updating  product items

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -3,12 +3,35 @@ import React from "react";
 export default function Books() {
   return (
   <main style={{ margin: '3vw 6vw' }}>
-    <div className="flex flex-col items-center max-w-3xs bg-gray-200 aspect-square p-2 rounded-lg ">
-      <h1 className="text-lg font-bold text-center">Book 1</h1>
-      <img className="w-40 h-50 rounded-lg" src="src/assets/images/book1.jpg" alt="Book 1" />
-      <p>Livro muito legal</p>
-      <p>R$ 255,99</p>
-      <p>6 em 1</p>
+    <div id="Itens" className="flex flex-wrap gap-15 justify-center">
+      <div className="flex flex-col items-center max-w-3xs bg-gray-200 aspect-square p-4 rounded-lg ">
+        <h1 className="text-lg font-bold text-center">Book 1</h1>
+        <img className="w-40 h-50 rounded-lg" src="src/assets/images/book1.jpg" alt="Book 1" />
+        <p>Livro muito legal</p>
+        <p>R$ 255,99</p>
+        <p>6 em 1</p>
+      </div>
+      <div className="flex flex-col items-center max-w-3xs bg-gray-200 aspect-square p-4 rounded-lg ">
+        <h1 className="text-lg font-bold text-center">Book 1</h1>
+        <img className="w-40 h-50 rounded-lg" src="src/assets/images/book1.jpg" alt="Book 1" />
+        <p>Livro muito legal</p>
+        <p>R$ 255,99</p>
+        <p>6 em 1</p>
+      </div>
+      <div className="flex flex-col items-center max-w-3xs bg-gray-200 aspect-square p-4 rounded-lg ">
+        <h1 className="text-lg font-bold text-center">Book 1</h1>
+        <img className="w-40 h-50 rounded-lg" src="src/assets/images/book1.jpg" alt="Book 1" />
+        <p>Livro muito legal</p>
+        <p>R$ 255,99</p>
+        <p>6 em 1</p>
+      </div>
+      <div className="flex flex-col items-center max-w-3xs bg-gray-200 aspect-square p-4 rounded-lg ">
+        <h1 className="text-lg font-bold text-center">Book 1</h1>
+        <img className="w-40 h-50 rounded-lg" src="src/assets/images/book1.jpg" alt="Book 1" />
+        <p>Livro muito legal</p>
+        <p>R$ 255,99</p>
+        <p>6 em 1</p>
+      </div>
     </div>
     </main>
   );
